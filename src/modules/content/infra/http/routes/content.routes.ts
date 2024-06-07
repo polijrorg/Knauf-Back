7035310d2d0404f1e9dadb386bd78785cf2b9a93
Joao Leal <joao.leal@polijunior.com.br>
2024-06-07 03:16:21 -0300

    adicionando coisas da aws (em desenvolvimento)

--- src/modules/content/infra/http/routes/content.routes.ts
+++ src/modules/content/infra/http/routes/content.routes.ts
@@ -1,9 +1,31 @@
-import { Router } from 'express';
+import { Router, response } from 'express';
 
 import ensureAuthenticated from '@shared/infra/middlewares/EnsureAuthenticated';
+
+import UploadImagesService from '@shared/container/providers/AWSProvider/aws_S3/implementations/UploadImagesService';
+
+import multer from 'multer';
+import multerConfig from '@shared/container/providers/AWSProvider/aws_S3/config/multer';
 import ContentController from '../controller/ContentController';
 
+
 const contentRoutes = Router();
+
+const upload = multer(multerConfig);
+
+contentRoutes.post('/uploadImageAWS', upload.single('image'), async (request, response) => {
+  const { file } = request;
+
+  if (!file) {
+    return response.status(400).json({ error: 'File is required' });
+  }
+
+  const uploadImagesService = new UploadImagesService();
+
+  await uploadImagesService.execute(file);
+
+  return response.send();
+});
 
 const contentController = new ContentController();
 
