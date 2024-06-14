14201f0891b4f794010ea0ec04caf2e1ce174cc1
Joao Leal <joao.leal@polijunior.com.br>
2024-06-14 10:01:09 -0300

    removendo migrações aws

--- src/shared/infra/prisma/migrations/20240614112633_end_aws/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `image` to the `Statment` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Statment" ADD COLUMN     "image" TEXT NOT NULL;
