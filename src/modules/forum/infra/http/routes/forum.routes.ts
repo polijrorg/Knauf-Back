8ea5c9b123019e3c7cb856e8f51aa5690b33b595
Pedro Gomes <pedro.oliveiragti@gmail.com>
2024-06-30 22:22:02 -0300

    Routes Update Status Comments And fORUM

--- src/modules/forum/infra/http/routes/forum.routes.ts
+++ src/modules/forum/infra/http/routes/forum.routes.ts
@@ -12,6 +12,8 @@ forumRoutes.get('/getAll', ensureAuthenticated, forumController.getAll);
 forumRoutes.delete('/delete/:idForum', ensureAuthenticated, forumController.delete);
 forumRoutes.post('/:idForum', ensureAuthenticated, forumController.addCommentsForum);
 forumRoutes.delete('/:idForum/:idComment', ensureAuthenticated, forumController.deleteComment);
+forumRoutes.post('/comment/updateStatus/:idComment', ensureAuthenticated, forumController.updateStatusComments);
+forumRoutes.delete('/forum/updateStatus/:idForum', ensureAuthenticated, forumController.updateStatusForum);
 forumRoutes.get('/search', forumController.getSearch);
 
 export default forumRoutes;
